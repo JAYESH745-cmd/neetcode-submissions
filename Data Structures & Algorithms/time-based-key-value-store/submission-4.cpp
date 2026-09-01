4f81adc4c61b4818287be8a238e4fc007fe495c1
Jayesh Arora <146444848+JAYESH745-cmd@users.noreply.github.com>
2026-09-01 14:00:39 +0530

    Add: time-based-key-value-store - submission-4

---
+++ Data Structures & Algorithms/time-based-key-value-store/submission-4.cpp
@@ -0,0 +1,31 @@
+class TimeMap {
+public:
+    TimeMap() {
+        
+    }
+    vector<int> time;
+    map<int,pair<string,string>> m;
+    
+    void set(string key, string value, int timestamp) {
+        if(m.find(timestamp)==m.end()) {
+            time.push_back(timestamp);
+            m[timestamp]={key,value};
+        }
+    }
+    
+    string get(string key, int tar) {
+       int l=0,r=time.size()-1;
+       int ans=-1;
+       while(l<=r){
+        int mid=l+(r-l)/2;
+        if(time[mid]<=tar){
+            ans=mid;
+            l=mid+1;
+        }
+        else r=mid-1;
+       }
+      
+       if(ans==-1) return "";
+       return m[time[ans]].second;
+    }
+};
