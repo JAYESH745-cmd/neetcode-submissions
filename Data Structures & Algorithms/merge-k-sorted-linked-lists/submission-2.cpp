1a1a4f6c1f68667e015eaee3623f27982ad73ab4
Jayesh Arora <146444848+JAYESH745-cmd@users.noreply.github.com>
2026-09-02 16:43:40 +0530

    Add: merge-k-sorted-linked-lists - submission-2

---
+++ Data Structures & Algorithms/merge-k-sorted-linked-lists/submission-2.cpp
@@ -0,0 +1,57 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+
+class Solution {
+public:
+    ListNode* merge(ListNode* list1,ListNode* list2){
+        ListNode* ans=new ListNode(-1);
+        ListNode* temp1=list1;
+        ListNode* temp2=list2;
+        ListNode* temp=ans;
+        while(temp1 && temp2){
+            if(temp1->val<temp2->val){
+                temp->next=temp1;
+                temp1=temp1->next;
+                temp=temp->next;
+            }
+            else {
+                temp->next=temp2;
+                temp2=temp2->next;
+                temp=temp->next;
+            }
+        }
+        while(temp1){
+            temp->next=temp1;
+            temp1=temp1->next;
+            temp=temp->next;
+        }
+        while(temp2){
+            temp->next=temp2;
+            temp2=temp2->next;
+            temp=temp->next;
+        }
+        return ans->next;
+    }
+    ListNode* mergeKLists(vector<ListNode*>& lists) {
+        int n=lists.size();
+        if(n==0) return NULL;
+        if(n==1) return lists[0];
+        while(lists.size()>1){
+            ListNode* a=lists[0];
+            lists.erase(lists.begin()+0);
+            ListNode* b=lists[0];
+            lists.erase(lists.begin()+0);
+            ListNode* m=merge(a,b);
+            lists.push_back(m);
+        }
+        return lists[0];
+    }
+};
