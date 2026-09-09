2fa13dbab69d964520eb591c6798638594ff4f3b
Jayesh Arora <146444848+JAYESH745-cmd@users.noreply.github.com>
2026-09-09 12:56:42 +0530

    Add: valid-binary-search-tree - submission-3

---
+++ Data Structures & Algorithms/valid-binary-search-tree/submission-3.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    bool isValidBST(TreeNode* root) {
+        TreeNode* curr=root;
+        vector<int> ans;
+        while(curr){
+            if(curr->left){
+                TreeNode* pred=curr->left;
+                while(pred->right && pred->right!=curr){
+                    pred=pred->right;
+                }
+                //linking
+                if(!pred->right){
+                    pred->right=curr;
+                    curr=curr->left;
+                }
+                else{
+                    pred->right=NULL;
+                    ans.push_back(curr->val);
+                    curr=curr->right;
+                }
+            }
+            else{
+                ans.push_back(curr->val);
+                curr=curr->right;
+            } 
+        }
+        for(int i=1;i<ans.size();i++){
+            if(ans[i-1]>=ans[i]) return false;
+        }
+        return true;
+    }
+};
